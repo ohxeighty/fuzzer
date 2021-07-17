829baa2f2843c20e60c588b5ed078aa56a82de08
Eric R Chang <changeric77@gmail.com>
2021-07-17 06:16:25 -0400

    Strats.

--- sourpatchfuzzer/main.py
+++ sourpatchfuzzer/main.py
@@ -4,11 +4,31 @@ from argparse import ArgumentParser
 from os.path import isfile
 from sys import exit, argv
 
+"""
+So this is what I think our code flow should look like:
+    main gets the binary and sample file
+    -> sample file is sent to parser
+    <- returns one or a list of bytes that are valid inputs
+    -> valid inputs are sent to one or more mutators
+    
+    ## then one of two options:
+    ## either 1:
+    <- mutated inputs are returned, along with a value for reach
+    -> mutated inputs are sent from main
+    ## or 2:
+    -> mutator sends inputs automatically adding pop using reach
+
+    1 is probably good enough for midpoint check-in
+    2 might be necessary for more complicated fuzzing techniques
+"""
 def fuzz(binary, sample):
     binls = harness.Harness(binary, sample) # not how this works - pls fix
     binls.spawn_process()
     binls.cont()
     binls.send(b"{}")
+
+
+
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='a warm and fuzzy feeling')
